34f48f34876933daecb5da0ecdaf6f4f9ad96a7d
Jihadpurnomo <90895465+Jihadpurnomo@users.noreply.github.com>
2021-09-24 16:47:18 +0700

    Create tugas no 2.cpp

---
+++ tugas no 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float s = 750;
+    float t = 20;
+    cout << "Program menghitung kecepatan\n";
+    cout << "Jarak tempuh : " << s << " km" << endl;
+    cout << "Waktu tempuh : " << t << " jam" << endl;
+    cout << "Hasilnya adalah : " << s / t << " km/jam" << endl;
+
+    return 0;
+}
